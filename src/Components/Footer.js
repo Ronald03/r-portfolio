dc0f80871d6f7eddd28ec433c6629c1c8cbe62b3
Ronald C <ronaldceballos15@gmail.com>
2020-02-26 21:11:54 -0500

    Worked on footer; still needs alignment

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,8 +4,23 @@ export default function Footer() {
   return (
     <div className="main-footer">
       <footer class="footer">
+
+        <div className="footer-trial name-on-footer fontSource center-content">
+        <span id="ronald">Ronald <span id="ceballos">Ceballos</span></span>
+        </div>
+        <div className="footer-trial footer-address center-content">
+        <span>Lewisville, TX 75056 <i class="fas fa-map-marker-alt"></i></span>
+        </div>
+        <div className="footer-trial foot3 social">
+          <a href="https://github.com/Ronald03">
+            <i class="fab fa-github"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/ronaldceballos/">
+            <i class="fab fa-linkedin"></i>
+          </a>
+        </div>
         
-        <div className="name-on-footer fontSource center-content">
+        {/* <div className="name-on-footer fontSource center-content">
           <span id="ronald">Ronald <span id="ceballos">Ceballos</span></span>
         </div>
 
@@ -14,7 +29,7 @@ export default function Footer() {
         </div>
 
         <div className="social center-content"><span>Social</span></div>
-
+ */}
        {/*
 
         <div class="footer-address ftr">
