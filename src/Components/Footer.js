9dee72dcdf4cbe3bcf914b0891a4b965ff845ccc
RonaldLnxDesk <ronaldceballos15@gmail.com>
2020-10-27 06:17:14 -0500

    early morning commit; post deployment

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,27 +2,28 @@ import React from "react";
 import "./Footer.css";
 
 export default function Footer() {
-  let footerHeight =document.getElementsByTagName("footer").offsetHeight;
+  let footerHeight = document.getElementsByTagName("footer").offsetHeight;
   console.log(footerHeight);
   return (
     <div className="main-footer">
       <footer class="footer">
-
         <div className=" name-on-footer fontSource center-content">
-        <span id="ronald">Ronald</span> <span >Ceballos</span>
+          <span id="ronald">Ronald</span> <span>Ceballos</span>
         </div>
         <div className="footer-address center-content">
-        <span>Lewisville, TX 75056 <i class="fas fa-map-marker-alt"></i></span>
+          <span>
+            Lewisville, TX 75056 <i class="fas fa-map-marker-alt"></i>
+          </span>
         </div>
         <div className="social center-content">
-          <a href="https://github.com/Ronald03">
+          <a href="https://github.com/Ronald03/r-portfolio">
             <i class="fab fa-github"></i>
           </a>
           <a href="https://www.linkedin.com/in/ronaldceballos/">
             <i class="fab fa-linkedin"></i>
           </a>
         </div>
-
       </footer>
-    </div>)
+    </div>
+  );
 }
